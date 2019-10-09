e58fb144ffa0923038ee9293bf0159fc7a56c22e
Greyy <briegrant416@gmail.com>
2019-10-09 11:59:54 +0300

    Fix typo in README file

---
+++ hello.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+
+   printf("This tutorial was completed by:\n\n");
+
+   printf("@eneskaya\n");
+   printf("@yogs-kum\n");
+   printf("@gkarlos\n");
+   printf("@roxierox\n");
+   printf("@santushet\n");
+   printf("@praveenkumarb\n");
+   printf("@mkeohane-cisco\n");
+   printf("@mario1ua\n");
+   printf("@muhammadhassan102\n");
+   printf("@pdubois\n");
+   printf("@Curt-T\n");
+   printf("@azru0512\n");
+   printf("@ddferrando\n");
+   printf("@luotaoyeah\n");
+   printf("@uthmanhere\n");
+   printf("@diszgaurav\n");
+   printf("@behpn\n");
+
+   printf("\nSend a PR to add your username here! :-) \n");
+
+   return 0;
+}
